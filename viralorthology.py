6c670d50862e737489059271d7ae2ad6f5a145a3
LFM <144485318+lfm-bio@users.noreply.github.com>
2024-04-19 12:57:05 -0300

    write log

--- viralorthology.py
+++ viralorthology.py
@@ -35,8 +35,11 @@ class Args:
         self.blastp = ''
         self.HMMsearch = ''
 
+    def get_software_params(self, software_params):
+        return software_params if software_params != '' else 'default'
+
     def get_all_params(self):
-        return f'proteinortho: {self.proteinortho}\norfinder: {self.orffinder}\nblastp: {self.blastp}\nHMMsearch: {self.HMMsearch}'
+        return f'proteinortho: {self.get_software_params(self.proteinortho)}\norfinder: {self.get_software_params(self.orffinder)}\nblastp: {self.get_software_params(self.blastp)}\nHMMsearch: {self.get_software_params(self.HMMsearch)}'
 
     def __repr__(self):
         return f'{self.proteinortho} {self.orffinder} {self.blastp} {self.HMMsearch}'
